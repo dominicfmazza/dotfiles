8d9fa54ab55bacfea646d1642b6f3709d760685b
Dominic Mazza <dominicfmazza@gmail.com>
2024-10-12 16:30:52 -0600

    fix(nvim): getting which-key to cooperate (hard mode)

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    version = "3.13.3",
     opts = {},
   },
   {
@@ -10,7 +11,21 @@ return {
     lazy = false,
     opts = {},
   },
-  { "numToStr/Comment.nvim" },
+  { "numToStr/Comment.nvim", lazy = false, keys = {
+  {
+    "<leader>/",
+    function()
+      require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
+    end,
+    desc = "Toggle linewise comment",
+    mode = "n"
+  },  {
+    "<leader>/",
+    "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+    desc = "Toggle block comment",
+    mode = "v"
+  }
+  } },
   {
     "nmac427/guess-indent.nvim",
     opts = {},
