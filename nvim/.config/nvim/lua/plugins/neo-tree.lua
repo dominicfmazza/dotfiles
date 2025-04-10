500c835d2ae56ddb4bb3692110989f0c1576d0a9
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-04-10 07:39:14 -0600

    nvim: formatting

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,17 +1,26 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    lazy = false,
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    keys = {
-      { "<leader>e", "<cmd>Neotree focus filesystem toggle<cr>", desc = "Filetree: Toggle" },
-      { "<leader>o", "<cmd>Neotree focus filesystem<cr>", desc = "Filetree: Focus" },
-    },
+  "nvim-neo-tree/neo-tree.nvim",
+  lazy = false,
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
   },
+  keys = {
+    { "<leader>e", "<cmd>Neotree focus filesystem toggle<cr>", desc = "Filetree: Toggle" },
+    { "<leader>o", "<cmd>Neotree focus filesystem<cr>", desc = "Filetree: Focus" },
+  },
+  config = function()
+    vim.diagnostic.config {
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.INFO] = "",
+          [vim.diagnostic.severity.HINT] = "󰌵",
+        },
+      },
+    }
+  end,
 }
-
