537990bc65dcb436d179747ef7e0f4fdebe2a4f6
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-04-10 07:39:54 -0600

    nvim: use mini.jump for better fFtT

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,4 @@
+return {
+  -- { "echasnovski/mini.pairs", lazy = false, version = "*", config = function() require("mini.pairs").setup() end },
+  { "echasnovski/mini.jump", lazy = false, version = "*", config = function() require("mini.jump").setup { delay = { highlight = 10 ^ 7 } } end },
+}
