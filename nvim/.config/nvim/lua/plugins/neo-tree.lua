9e07f8c3159dbc366bf84f4a3711faac55ba2738
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-04-19 12:39:34 -0600

    nvim(neo-tree): finally bypassed the hiding of dotfiles

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -21,5 +21,12 @@ return {
         },
       },
     }
+    require("neo-tree").setup {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    }
   end,
 }
