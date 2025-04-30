bb83ea9aeadd3784a77ba776802c850b054f515f
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-04-29 21:19:49 -0600

    nvim(python): uv instead of venv-selector

--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -1,17 +1,8 @@
 return {
   {
-    "linux-cultist/venv-selector.nvim",
-    opts = {
-      -- Your options go here
-      -- name = "venv",
-      -- auto_refresh = false
-    },
+    "benomahony/uv.nvim",
     lazy = false,
-    branch = "regexp",
-    keys = {
-      -- Keymap to open VenvSelector to pick a venv.
-      { "<leader>vs", "<cmd>VenvSelect<cr>" },
-    },
+    config = function() require("uv").setup() end,
   },
   {
     "mfussenegger/nvim-dap-python",
