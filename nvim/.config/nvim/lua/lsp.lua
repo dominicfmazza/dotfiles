5e920b5cfbc0feba8d7d836ae566dccf1354c229
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-04-29 21:22:50 -0600

    nvim(lsp): properly setting up capabilites with blink cmp

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,16 +1,18 @@
 local wk = require "which-key"
 local snacks = require "snacks"
 
-vim.lsp.config("*", {
-  capabilities = {
-    workspace = { didChangeWatchedFiles = { dynamicRegistration = true } },
-    textDocument = {
-      foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
-      },
+local lsp_capabilities = {
+  workspace = { didChangeWatchedFiles = { dynamicRegistration = true } },
+  textDocument = {
+    foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
     },
   },
+}
+
+vim.lsp.config("*", {
+  capabilities = require("blink.cmp").get_lsp_capabilities(lsp_capabilities),
 })
 
 local lsps = {
@@ -26,25 +28,45 @@ local lsps = {
 vim.lsp.enable(lsps)
 
 wk.add {
-  { "gr", group = "+LSP" },
-  {
-    "grf",
-    function() require("conform").format { async = true, lsp_fallback = true } end,
-    desc = "Format",
-  },
-  { "grn", function() vim.lsp.buf.rename() end, desc = "LSP: Rename" },
-  { "grd", function() snacks.picker.lsp_definitions() end, desc = "LSP: Goto Definition" },
-  { "grD", function() snacks.picker.lsp_declarations() end, desc = "LSP: Goto Declaration" },
-  { "grr", function() snacks.picker.lsp_references() end, nowait = true, desc = "LSP: References" },
-  { "gri", function() snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-  { "grt", function() snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
-  { "grs", function() snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
-  { "grS", function() snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
-  { "gra", vim.lsp.buf.code_action, desc = "LSP: Code Action" },
-  { "<C-k>", vim.lsp.buf.hover, desc = "LSP: Hover LSP Help" },
-  { "<C-s>", vim.lsp.buf.signature_help, desc = "LSP Signature Help" },
-  noremap = true,
+  { "<leader>l", group = "+LSP" },
 }
+
+vim.api.nvim_create_autocmd("LspDetach", {
+  callback = function(args)
+    -- Get the detaching client
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    -- Remove the autocommand to format the buffer on save, if it exists
+    if client:supports_method "textDocument/formatting" then vim.api.nvim_clear_autocmds {
+      event = "BufWritePre",
+      buffer = args.buf,
+    } end
+  end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    wk.add {
+      {
+        "<leader>lf",
+        function() require("conform").format { async = true, lsp_fallback = true } end,
+        desc = "Format",
+      },
+      { "<leader>ln", function() vim.lsp.buf.rename() end, desc = "LSP: Rename" },
+      { "<leader>ld", function() snacks.picker.lsp_definitions() end, desc = "LSP: Goto Definition" },
+      { "<leader>lD", function() snacks.picker.lsp_declarations() end, desc = "LSP: Goto Declaration" },
+      { "<leader>lr", function() snacks.picker.lsp_references() end, nowait = true, desc = "LSP: References" },
+      { "<leader>li", function() snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+      { "<leader>lt", function() snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+      { "<leader>ls", function() snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+      { "<leader>lS", function() snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+      { "<leader>a", '<cmd>lua require("fastaction").code_action()<CR>', desc = "LSP: Code Action" },
+      { "<C-k>", vim.lsp.buf.hover, desc = "LSP: Hover LSP Help" },
+      { "<C-s>", vim.lsp.buf.signature_help, desc = "LSP Signature Help" },
+      buffer = args.buf,
+      noremap = true,
+    }
+  end,
+})
 
 vim.api.nvim_create_user_command("LspInfo", ":checkhealth vim.lsp", { desc = "Alias to `:checkhealth vim.lsp`" })
 
@@ -97,3 +119,36 @@ vim.api.nvim_create_user_command("LspLog", function()
   local log = vim.lsp.log.get_filename()
   vim.cmd("e " .. log)
 end, {})
+
+---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+local progress = vim.defaulttable()
+vim.api.nvim_create_autocmd("LspProgress", {
+  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+    if not client or type(value) ~= "table" then return end
+    local p = progress[client.id]
+
+    for i = 1, #p + 1 do
+      if i == #p + 1 or p[i].token == ev.data.params.token then
+        p[i] = {
+          token = ev.data.params.token,
+          msg = ("[%3d%%] %s%s"):format(value.kind == "end" and 100 or value.percentage or 100, value.title or "", value.message and (" **%s**"):format(value.message) or ""),
+          done = value.kind == "end",
+        }
+        break
+      end
+    end
+
+    local msg = {} ---@type string[]
+    progress[client.id] = vim.tbl_filter(function(v) return table.insert(msg, v.msg) or not v.done end, p)
+
+    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+    vim.notify(table.concat(msg, "\n"), "info", {
+      id = "lsp_progress",
+      title = client.name,
+      opts = function(notif) notif.icon = #progress[client.id] == 0 and " " or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1] end,
+    })
+  end,
+})
