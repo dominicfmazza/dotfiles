7f68122360f5e30b6316f3c3c5db2cf04dd46dd6
Dominic Mazza <dominicfmazza@gmail.com>
2025-06-07 08:32:46 -0600

    wezterm: removed the hosts configuration for now

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -23,8 +23,6 @@ config.window_frame = {
   font_size = 13,
 }
 
-require("~/.config/environments/wezterm/hosts.lua").configure_hosts(config)
-
 wezterm.on('update-status', function(window)
   local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 
