e2c0f8925cd59516b733d4f3c7171aacacf0e250
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-07-14 09:35:06 -0600

    nvim: added diagnostic and statuscolumn support

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -15,4 +15,39 @@ return {
     opts = {},
   },
   { "yochem/jq-playground.nvim", cmd = { "JqPlayground" } },
+  {
+    {
+      "rachartier/tiny-inline-diagnostic.nvim",
+      event = "VeryLazy", -- Or `LspAttach`
+      priority = 1000, -- needs to be loaded in first
+      config = function()
+        require("tiny-inline-diagnostic").setup()
+        vim.diagnostic.config { virtual_text = false } -- Only if needed in your configuration, if you already have native LSP diagnostics
+      end,
+    },
+  },
+  {
+    "luukvbaal/statuscol.nvim",
+    lazy = false,
+    dependencies = { "lewis6991/gitsigns.nvim" },
+    config = function()
+      -- local builtin = require("statuscol.builtin")
+      require("statuscol").setup {
+        -- configuration goes here, for example:
+        -- relculright = true,
+        -- segments = {
+        --   { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        --   {
+        --     sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
+        --     click = "v:lua.ScSa"
+        --   },
+        --   { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+        --   {
+        --     sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+        --     click = "v:lua.ScSa"
+        --   },
+        -- }
+      }
+    end,
+  },
 }
