12518434373f15ae95b200c057c0a1852f63640f
Dominic Mazza <dominicfmazza@gmail.com>
2025-08-24 14:46:47 -0600

    wezterm: enable texture healing

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -9,45 +9,45 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 13
-config.color_scheme = 'Material Darker (base16)'
+config.font_size = 14
+config.color_scheme = "Material Darker (base16)"
 config.colors = {
-  background = '#0f0f0f'
+	background = "#0f0f0f",
 }
 
-
 config.font = wezterm.font({
-  family = "MonaspiceAr Nerd Font",
-    weight = "Medium"
+	family = "MonaspiceAr Nerd Font",
+	weight = "Medium",
+	harfbuzz_features = { "calt" },
 })
 
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 config.window_frame = {
-  font = wezterm.font({ family = 'MonaspiceAr Nerd Font', weight = 'Bold' }),
-  font_size = 13,
+	font = wezterm.font({ family = "MonaspiceAr Nerd Font", weight = "Bold" }),
+	font_size = 13,
 }
 
 local status, hosts = pcall(require, "hosts")
 
 if status then
-  hosts.configure_hosts(config)
+	hosts.configure_hosts(config)
 end
 
-wezterm.on('update-status', function(window)
-  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+wezterm.on("update-status", function(window)
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 
-  local color_scheme = window:effective_config().resolved_palette
-  local bg = color_scheme.background
-  local fg = color_scheme.foreground
+	local color_scheme = window:effective_config().resolved_palette
+	local bg = color_scheme.background
+	local fg = color_scheme.foreground
 
-  window:set_right_status(wezterm.format({
-    { Background = { Color = 'none' } },
-    { Foreground = { Color = bg } },
-    { Text = SOLID_LEFT_ARROW },
-    { Background = { Color = bg } },
-    { Foreground = { Color = fg } },
-    { Text = ' ' .. wezterm.hostname() .. ' ' },
-  }))
+	window:set_right_status(wezterm.format({
+		{ Background = { Color = "none" } },
+		{ Foreground = { Color = bg } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = bg } },
+		{ Foreground = { Color = fg } },
+		{ Text = " " .. wezterm.hostname() .. " " },
+	}))
 end)
 
 return config
