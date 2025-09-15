480c6e0fb9b51492f4f2f4e8f5cfd89d68a3b475
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-09-15 06:23:10 -0600

    nvim: removed lazy path from the luals configuration

--- nvim/.config/nvim/lsp/luals.lua
+++ nvim/.config/nvim/lsp/luals.lua
@@ -12,7 +12,6 @@ return {
           [vim.fn.expand "$VIMRUNTIME/lua"] = true,
           [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
           [vim.fn.expand "$HOME/.config/nvim"] = true,
-          [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
         },
         maxPreload = 100000,
         preloadFileSize = 10000,
