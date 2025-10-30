61ad0a96eab46d79e42b37079fc396a4755d3998
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-10-30 16:34:57 -0600

    fix: remove dumb autocommands

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -1,26 +1,5 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
-
-vim.api.nvim_create_autocmd('BufWinEnter', {
-  pattern = { '*.md' },
-  callback = function()
-    vim.opt.colorcolumn = '80'
-    vim.opt.textwidth = 80
-    vim.opt.linebreak = false
-  end,
-})
-
-vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
-  pattern = { '*.md' },
-  callback = function()
-    vim.opt.colorcolumn = '120'
-    vim.opt.textwidth = 120
-    vim.opt.linebreak = true
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function() vim.highlight.on_yank() end,
 })
