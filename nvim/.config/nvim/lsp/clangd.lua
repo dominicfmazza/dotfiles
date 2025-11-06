99170d2bc3a99e45639737b1bf8c49a246aedf52
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-11-06 15:26:10 -0700

    fix: turn on clang features again

--- nvim/.config/nvim/lsp/clangd.lua
+++ nvim/.config/nvim/lsp/clangd.lua
@@ -7,12 +7,12 @@ return {
   },
   filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
   root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "build/compile_commands.json", "compile_flags.txt", "configure.ac", ".git" },
-  -- capabilities = {
-  --   textDocument = {
-  --     completion = {
-  --       editsNearCursor = true,
-  --     },
-  --   },
-  --   offsetEncoding = { "utf-8", "utf-16" },
-  -- },
+  capabilities = {
+    textDocument = {
+      completion = {
+        editsNearCursor = true,
+      },
+    },
+    offsetEncoding = { "utf-8", "utf-16" },
+  },
 }
