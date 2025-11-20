4036a00399e071ef1db93c56ac6c16582431c373
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-11-20 08:49:28 -0700

    nvim: autoresize panes and disable semantic tokens

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -63,14 +63,15 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
+vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if not client == nil then client.server_capabilities.semanticTokensProvider = nil end
+    if client and client.server_capabilities then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
   end,
 })
-
 -- Auto resize splits when the terminal's window is resized
 vim.api.nvim_create_autocmd("VimResized", {
-    command = "wincmd =",
+  command = "wincmd =",
 })
