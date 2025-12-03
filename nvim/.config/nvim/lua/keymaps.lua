abe4d874f814fedea4de06b46bbe17fdd1bff10a
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-12-03 07:36:23 -0700

    nvim: ensure window keymaps work when in terminal buffer

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,8 +1,8 @@
 local map = vim.keymap.set
-map("n", "<M-h>", "<C-w>h")
-map("n", "<M-j>", "<C-w>j")
-map("n", "<M-k>", "<C-w>k")
-map("n", "<M-l>", "<C-w>l")
+map({"n", "x", "t"},  "<M-h>", "<C-w>h")
+map({"n", "x", "t"},  "<M-j>", "<C-w>j")
+map({"n", "x", "t"},  "<M-k>", "<C-w>k")
+map({"n", "x", "t"},  "<M-l>", "<C-w>l")
 
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
 map("n", "<C-[>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
