45a51ad0f0f702ccb243b3e9b422b791b2e648b4
Mazza, Dominic F. <dominic.f.mazza@lmco.com>
2025-12-03 07:38:07 -0700

    nvim(term): floating and lazygit terminals now are toggle based as opposed to close and open

--- nvim/.config/nvim/lua/term.lua
+++ nvim/.config/nvim/lua/term.lua
@@ -13,24 +13,47 @@ require("ergoterm").setup {
 }
 
 local map = vim.keymap.set
+local ergoterm = require "ergoterm"
 
 -- Terminal creation with different layouts
 map("n", "<leader>ts", ":TermNew layout=below<CR>", { noremap = true, silent = true, desc = "Open below" })
 map("n", "<leader>tv", ":TermNew layout=right<CR>", { noremap = true, silent = true, desc = "Open right" })
-map("n", "<leader>tf", ":TermNew layout=float<CR>", { noremap = true, silent = true, desc = "Open float" })
 map("n", "<leader>tt", ":TermNew layout=tab<CR>", { noremap = true, silent = true, desc = "Open tab" })
 
 -- Open terminal picker
 map("n", "<leader>tl", ":TermSelect<CR>", { noremap = true, silent = true, desc = "Terminal picker" })
 
 -- Send text to last focused terminal
-map({"n", "x"}, "<leader>ts", ":TermSend! new_line=false<CR>", { noremap = true, silent = true, desc = "Send text to last focused terminal" })
+map({ "n", "x" }, "<leader>ts", ":TermSend! new_line=false<CR>", { noremap = true, silent = true, desc = "Send text to last focused terminal" })
 
 -- Send and show output without focusing terminal
-map({"n", "x"}, "<leader>tx", ":TermSend! action=open<CR>", { noremap = true, silent = true, desc = "Send without focusing" })
+map({ "n", "x" }, "<leader>tx", ":TermSend! action=open<CR>", { noremap = true, silent = true, desc = "Send without focusing" })
 
 map({ "x", "n", "t" }, "<M-'>", "<cmd>tabNext<CR>", { noremap = true, silent = true, desc = "" })
 map({ "x", "n", "t" }, "<M-;>", "<cmd>tabprevious<CR>", { noremap = true, silent = true, desc = "" })
-map({ "x", "n", "t" }, "<M-g>", "<cmd>TermNew name=lazygit layout=float cmd=lazygit<CR>", { noremap = true, silent = true, desc = "" })
+map({ "x", "n", "t" }, "<M-g>", function()
+  local server = ergoterm.find(function(term) return term.name == "lazygit" end)
+  if server then
+    server:toggle()
+  else
+    ergoterm:new {
+      layout = "float",
+      name = "lazygit",
+      cmd = "lazygit",
+      cleanup_on_success = true,
+    }
+  end
+end, { noremap = true, silent = true, desc = "" })
+map({ "x", "n", "t" }, "<M-f>", function()
+  local server = ergoterm.find(function(term) return term.name == "floating" end)
+  if server then
+    server:toggle()
+  else
+    ergoterm:new {
+      layout = "float",
+      name = "floating",
+    }
+  end
+end, { noremap = true, silent = true, desc = "Open float" })
 
-map("t", "<Esc><Esc>", [[<C-\><C-n>]], { noremap = true, silent = true, desc = "" })
+map("t", "<C-q>", [[<C-\><C-n>]], { noremap = true, silent = true, desc = "" })
